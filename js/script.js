a8fc45b4484d15f8c3eb731f652e83ce392197ae
Lebedenko Tatiana <lebedenko_tatiana@mail.ru>
2016-10-05 11:03:15 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,41 @@
+﻿
+var my_pow = function(x, n) {
+  var result = x;
+
+  if (n > 0) {
+    for (var i = 1; i < n; i++) {
+      result *= x;
+    }
+  }
+
+  if (n < 0 && x != 0) {
+    for (var i = -1; i > n; i--) {
+      result *= x;
+    }
+    result = 1 / result;
+  }
+
+  if (n == 0) {
+    result = 1;
+  }
+
+  if (n < 0 && x == 0) {
+    result = 'incurrect values!';
+  }
+    
+  return result;
+}
+
+
+  console.log('5 в степени 0 =  ', my_pow(5, 0));
+  console.log('2 в степени -1 =  ', my_pow(2, -1));
+  console.log('-2 в степени -2 =  ', my_pow(-2, -2));
+  console.log('0 в степени -1 =  ', my_pow(0, -1));
+
+
+  module.exports = my_pow;
+
+
+
+
+
---
+++ spec/spec.js
@@ -0,0 +1,27 @@
+﻿var test = require('../js/script.js'); 
+
+describe("test my_pow", function() {
+
+  it("call function my_pow for exponent 0", function() {
+    var result;
+    result = test(5, 0);
+    expect(result).toBe(1);
+  });
+
+  it("call function my_pow for negative exponent", function() {
+    result = test(2, -1);
+    expect(result).toBe(0.5);
+  });
+
+  it("call function my_pow for negative exponent and negative value", function() {
+    result = test(-2, -2);
+    expect(result).toBe(0.25);
+  });
+
+  it("call function my_pow for negative exponent and value = 0", function() {
+    var result = test(0, -1);
+    expect(result).toBe('incurrect values!');
+  });
+
+});
+
